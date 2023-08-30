e284174d366d469982dd1b5ab104a0c7ab98c72e
Victoria Kalenik-Harris <vk781@hotmail.com>
2023-08-30 14:46:36 +0100

    Add answers to jQuery Selectors Challenge

---
+++ 02-jQueryIntroduction/03-jquery-selectors/challenge-solution/jQuerySelectors_myAnswers.js
@@ -0,0 +1,26 @@
+// All images, based on the element type
+$("img");
+
+// The card_image class
+$(".card_image");
+
+// The footer, using its id
+$("#my_footer");
+
+// All paragraph tags within the footer
+$("footer p");
+
+// All header elements (<h1> through <h6>)
+$(":header");
+
+// The first element on the page
+$(":first");
+
+// The last div on the page
+$("div:last");
+
+// The last image on the page
+$("img:last");
+
+// All elements with an href attribute
+$("[href]");
